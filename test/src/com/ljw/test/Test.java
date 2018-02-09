4e53dccd8f870795f623efba92ea0ee2af493b93
liangjunwu@aspirecn.com <liangjunwu@aspirecn.com>
2018-02-09 14:11:22 +0800

    首次提交代码

---
+++ test/src/com/ljw/test/Test.java
@@ -0,0 +1,9 @@
+package com.ljw.test;
+
+public class Test {
+    public static void main(String[] args){
+        for (int j = 0; j < 100; j++) {
+            System.out.println(j);
+        }
+    }
+}
