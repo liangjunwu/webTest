c88fbbacde50ce0c75b7f78d2ea90b33e93022bf
liangjunwu@aspirecn.com <liangjunwu@aspirecn.com>
2018-02-09 14:21:58 +0800

    修改输出代码

--- test/src/com/ljw/test/Test.java
+++ test/src/com/ljw/test/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args){
         for (int j = 0; j < 100; j++) {
             System.out.println(j);
+            System.out.println("测试");
         }
     }
 }
